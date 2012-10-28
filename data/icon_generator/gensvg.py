718203af15c056dda783b2738eafae5475757d88
Sergio Costas <raster@rastersoft.com>
2012-10-28 15:26:02 +0100

    Added instructions to the GENSVG program

--- data/icon_generator/gensvg.py
+++ data/icon_generator/gensvg.py
@@ -3,16 +3,20 @@
 import sys
 import os
 
-
-
 if (len(sys.argv)!=2):
     print "Usage: gensvg input_file"
+    print "Example: gensvg cronopete_arrow_1"
     sys.exit()
-    
+
+# First is the name of the icon; second the inner color for the arrow; third the outline color for the arror; fourth the fill color for the container circle
+
 colors={"white":["#234567","#234567","#ffffff"], "red":["#ff0000","#ff0000","#ffffff"], "green":["#00ff00","#00ff00","#ffffff"], "yellow":["#ffc000","#ffc000","#ffffff"], "orange":["#ff8000","#ff8000","#ffffff"]}
-sizes={"16":18, "22":13, "24":12 ,"32":11 , "48":10 ,"64":8, "128":4}
+
+# DEFAULT_SIZE contains the outline width for the arror
 default_size=18
 
+# If do_sizes is true, will also generate PNGs in sizes 16, 22, 24, 32, 48, 64 and 128 pixels. SIZES contains the outline width for the arrow, for each size
+sizes={"16":18, "22":13, "24":12 ,"32":11 , "48":10 ,"64":8, "128":4}
 do_sizes=False
 
 fileini=open(sys.argv[1]+".svg","r")
