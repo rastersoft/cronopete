aa68ad97163a18fa8126f2f5c4fd672f4ae08faf
Sergio Costas <raster@rastersoft.com>
2012-10-28 15:30:51 +0100

    Now generates all the icons with one call

--- data/icon_generator/gensvg.py
+++ data/icon_generator/gensvg.py
@@ -5,7 +5,7 @@ import os
 
 if (len(sys.argv)!=2):
     print "Usage: gensvg input_file"
-    print "Example: gensvg cronopete_arrow_1"
+    print "Example: gensvg cronopete_arrow"
     sys.exit()
 
 # First is the name of the icon; second the inner color for the arrow; third the outline color for the arror; fourth the fill color for the container circle
@@ -19,28 +19,31 @@ default_size=18
 sizes={"16":18, "22":13, "24":12 ,"32":11 , "48":10 ,"64":8, "128":4}
 do_sizes=False
 
-fileini=open(sys.argv[1]+".svg","r")
-picture=fileini.read()
-fileini.close()
+for number in [1,2,3,4]:
 
-fname=sys.argv[1]
-for color in colors:
+    fname=sys.argv[1]+"_"+str(number)
 
-    if do_sizes:
-        for size in sizes:
-            newpic=picture[:]
-            newpic=newpic.replace("fill:#234567","fill:"+colors[color][0]).replace("stroke:#123456","stroke:"+colors[color][1]).replace("stroke-width:20;","stroke-width:"+str(sizes[size])+";")
-            fname2=fname+"_"+color+"_"+size+"x"+size
-            fileout=open("tmp.svg","w")
-            fileout.write(newpic)
-            fileout.close()
-            command="inkscape -f tmp.svg -e "+fname2+".png -C -w "+size+" -h "+size
-            os.system(command)
-            os.system("rm tmp.svg")
+    fileini=open(fname+".svg","r")
+    picture=fileini.read()
+    fileini.close()
 
-    newpic=picture[:]
-    newpic=newpic.replace("fill:#345678","fill:"+colors[color][2]).replace("fill:#234567","fill:"+colors[color][0]).replace("stroke:#123456","stroke:"+colors[color][1]).replace("stroke-width:20;","stroke-width:"+str(default_size)+";")
-    fname3=fname+"_"+color
-    fileout=open(fname3+".svg","w")
-    fileout.write(newpic)
-    fileout.close()
+    for color in colors:
+
+        if do_sizes:
+            for size in sizes:
+                newpic=picture[:]
+                newpic=newpic.replace("fill:#234567","fill:"+colors[color][0]).replace("stroke:#123456","stroke:"+colors[color][1]).replace("stroke-width:20;","stroke-width:"+str(sizes[size])+";")
+                fname2=fname+"_"+color+"_"+size+"x"+size
+                fileout=open("tmp.svg","w")
+                fileout.write(newpic)
+                fileout.close()
+                command="inkscape -f tmp.svg -e "+fname2+".png -C -w "+size+" -h "+size
+                os.system(command)
+                os.system("rm tmp.svg")
+
+        newpic=picture[:]
+        newpic=newpic.replace("fill:#345678","fill:"+colors[color][2]).replace("fill:#234567","fill:"+colors[color][0]).replace("stroke:#123456","stroke:"+colors[color][1]).replace("stroke-width:20;","stroke-width:"+str(default_size)+";")
+        fname3=fname+"_"+color
+        fileout=open(fname3+".svg","w")
+        fileout.write(newpic)
+        fileout.close()
